c5be2471c1b3b0a1e89b0dee22fedb2d11bb6bcc
JoahvanderSloot <144343776+JoahvanderSloot@users.noreply.github.com>
2025-05-07 12:08:19 +0200

    player done

---
+++ Assets/_Project/Dev/Scripts/Player.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] InputActionReference tapAction;
+    [SerializeField] InputActionReference positionAction;
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] float startYpos;
+
+    GameObject currentBall;
+    GameObject currentLine;
+    bool isDragging = false;
+    bool canDrag = true;
+    bool isSpawning = false;
+
+    private void OnEnable()
+    {
+        tapAction.action.performed += OnTap;
+        tapAction.action.canceled += OnRelease;
+        tapAction.action.Enable();
+        positionAction.action.Enable();
+
+        StartCoroutine(SpawnNewBall());
+    }
+
+    private void OnDisable()
+    {
+        tapAction.action.performed -= OnTap;
+        tapAction.action.canceled -= OnRelease;
+        tapAction.action.Disable();
+        positionAction.action.Disable();
+    }
+
+    private void OnTap(CallbackContext _context)
+    {
+        if (!canDrag || currentBall == null) return;
+
+        Vector2 _screenPos = positionAction.action.ReadValue<Vector2>();
+        Vector3 _worldPos = Camera.main.ScreenToWorldPoint(new Vector3(_screenPos.x, _screenPos.y, 10f));
+
+        currentBall.transform.position = new Vector3(_worldPos.x, startYpos, 0f);
+        isDragging = true;
+    }
+
+    private void OnRelease(CallbackContext _context)
+    {
+        if (currentBall == null || isSpawning) return;
+
+        isDragging = false;
+        canDrag = false;
+        currentBall.GetComponent<Rigidbody2D>().gravityScale = 1f;
+        currentBall.GetComponent<BoxCollider2D>().isTrigger = false;
+        currentBall = null;
+
+        StartCoroutine(SpawnNewBall());
+    }
+
+    private IEnumerator SpawnNewBall()
+    {
+        isSpawning = true;
+        yield return new WaitForSeconds(0.2f);
+
+        currentBall = Instantiate(ballPrefab, new Vector3(0f, startYpos, 0f), Quaternion.identity);
+        currentBall.GetComponent<BoxCollider2D>().isTrigger = true;
+        currentLine = currentBall.transform.GetChild(0).gameObject;
+        currentLine.SetActive(false);
+
+        canDrag = true;
+        isSpawning = false;
+    }
+
+    private void Update()
+    {
+        if (isDragging && currentBall != null)
+        {
+            Vector2 _screenPos = positionAction.action.ReadValue<Vector2>();
+            Vector3 _worldPos = Camera.main.ScreenToWorldPoint(new Vector3(_screenPos.x, _screenPos.y, 10f));
+
+            currentBall.transform.position = new Vector3(_worldPos.x, startYpos, 0f);
+
+            if(currentLine != null)
+            {
+                currentLine.SetActive(true);
+            }
+        }
+        else
+        {
+            if (currentLine != null)
+            {
+                currentLine.SetActive(false);
+            }
+        }
+    }
+}
