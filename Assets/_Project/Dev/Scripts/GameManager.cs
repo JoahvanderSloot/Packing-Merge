6266ee0a2a71234bdc44b1493f1aadbd473a0576
JoahvanderSloot <144343776+JoahvanderSloot@users.noreply.github.com>
2025-05-08 11:35:31 +0200

    game over menu

--- Assets/_Project/Dev/Scripts/GameManager.cs
+++ Assets/_Project/Dev/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject nextDropDisplay;
     [SerializeField] GameObject[] dropPrefabs;
     [SerializeField] Player player;
-    [SerializeField] GameOver;
+    [SerializeField] GameObject GameOverMenu;
 
     [SerializeField] TextMeshProUGUI scoreText;
 
